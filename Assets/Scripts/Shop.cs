956d694dafeee3f414ebed6f2795798fe536d25a
chicks427 <chicks427@live.co.uk>
2021-05-21 19:59:14 +0100

    Added shop UI and script.

---
+++ Assets/Scripts/Shop.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+	public void PurchaseStandardTurret()
+	{
+		Debug.Log("Standard turret purchased");
+	}
+
+	public void PurchaseAnotherTurret()
+	{
+		Debug.Log("Another turret purchased");
+	}
+}
