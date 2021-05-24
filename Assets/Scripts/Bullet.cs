67e0e359f0c86deed6fce28a8c2c99bb452ed6e0
chicks427 <chicks427@live.co.uk>
2021-05-24 05:40:49 +0100

    Added missle prefab.
    Has the 3d object of a missle rather than standard bullet now. Implemented LookAt() functionality so it appears to chase the enemy.

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -33,6 +33,7 @@ public class Bullet : MonoBehaviour
         }
 
         transform.Translate(dir.normalized * distanceThisFrame, Space.World);
+        transform.LookAt(target);
     }
 
     void HitTarget()
