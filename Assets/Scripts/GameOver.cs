fbe9e2f9a9341be150a9f88cf8bf346d6e0e670a
chicks427 <chicks427@live.co.uk>
2021-06-30 02:00:43 +0100

    Refactored code displaying rounds survived into separate script.
    This will be useful when we later implement a 'count up' animation on the number, especially as we are using this in two places now; on the game over and the level won screen.

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -6,14 +6,8 @@ using UnityEngine.SceneManagement;
 
 public class GameOver : MonoBehaviour
 {
-	public Text roundsText;
 	public SceneFader sceneFader;
 	public string mainMenuName = "MainMenu";
-
-	void OnEnable()
-	{
-		roundsText.text = PlayerStats.Rounds.ToString();
-	}
 
 	public void Retry()
 	{
---
+++ Assets/Scripts/RoundsSurvived.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RoundsSurvived : MonoBehaviour
+{
+    public Text roundsText;
+
+    void OnEnable()
+	{
+		roundsText.text = PlayerStats.Rounds.ToString();
+	}
+}
